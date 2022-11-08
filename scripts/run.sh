Xilinx-CNS/cns-sapi-ts

commit 0a36760536a196deed7691c5ca3ece33df477c1c

scripts: detect NIC firmware variant in a suitable location
These checks can be painlessly postponed until the necessary
variables are set.

OL-Redmine-Id: 12265
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Acked-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

conf/scripts/sockapi-ts
@@ -16,6 +16,16 @@
 . ${SF_TS_CONFDIR}/scripts/ipvlan
 . ${SF_TS_CONFDIR}/scripts/sfptpd
 
+source "${TE_BASE}/scripts/lib"
+file_to_src="${TE_TS_RIGSDIR}/scripts/lib/ts_funcs"
+[[ -f "$src_file" ]] &&  source "$src_file"
+
+#
+# The called function can export the following variables:
+# LIBBPF_SRC, SF_TS_PINGPONG_IUT and SF_TS_PINGPONG_TST.
+#
+call_if_defined export_sapi_ts_site_envs
+
 guess_libdir="/usr/lib"
 
 #######################################
@@ -362,5 +372,15 @@ if ! ${ST_IUT_IS_CMOD} ; then
             echo "Add 'NOZEROCONF=yes' line to /etc/sysconfig/network to disable ZEROCONF."
             exit 1
         done
+    fi
+
+    if [[ -z "$SF_TS_CMDCLIENT" ]] ; then
+        call_if_defined export_cmdclient "$TE_IUT"
+    fi
+
+    # Note: firmware variants (full/low) applicable for sfc driver only
+    if [[ "$TE_ENV_IUT_NET_DRIVER" == "sfc" ]] ; then
+        export_iut_fw_version "$TE_IUT" "$TE_IUT_TST1"
+        check_fw_var_consistency
     fi
 fi

scripts/run.sh
@@ -81,7 +81,6 @@ RUN_OPTS="${RUN_OPTS} --tester-only-req-logues"
 ST_IGNORE_NM=false
 ST_IGNORE_ZEROCONF=false
 ST_IUT_IS_CMOD=false
-is_nonsf=false
 cfg_sfx=""
 while test -n "$1" ; do
     if call_if_defined grab_cfg_check_opt "$1" ; then
@@ -134,7 +133,7 @@ while test -n "$1" ; do
         # Use cfg without '-mlx' as hostname
         hostname="${cfg/%-mlx/}"
         if test "x$hostname" != "x$cfg" ; then
-            is_mlx=true
+            cfg_sfx="${cfg/${hostname}-/}"
         fi
 
         RUN_OPTS="${RUN_OPTS} --opts=run/$cfg"
@@ -225,20 +224,6 @@ OOL_SET=$(${RUNDIR}/scripts/ool_fix_consistency.sh $hostname "$cfg_sfx" $OOL_SET
 AUX_REQS=$(${RUNDIR}/scripts/ool_fix_reqs.py --ools="$OOL_SET" --cfg_sfx="$cfg_sfx")
 RUN_OPTS="${RUN_OPTS} ${AUX_REQS}"
 
-if ! $ST_IUT_IS_CMOD && ! $is_nonsf ; then
-    export_cmdclient $hostname
-
-    # Note: firmware variants (full/low) applicable for sfc only
-    iut_ifs=( $(get_sfx_ifs $hostname sfc "") )
-    export_iut_fw_version $hostname ${iut_ifs[0]}
-fi
-
-if ! $is_mlx ; then
-    OOL_SET=$(fw_var_consistency $OOL_SET)
-    if test $? -eq 1 ; then
-        exit 1
-    fi
-fi
 RUN_OPTS="$RUN_OPTS $OOL_PROFILE"
 for i in $OOL_SET ; do
     if [[ "$i" == "hwport2" ]] ; then

oktetlabs/sapi-ts

commit 97af2b650a08aef1c18dc949e260f5243e13160f

ts: use the file sourcing mechanism provided by TE
This simplifies the code and avoids typos.

OL-Redmine-Id: 12265
Fixes: 0a36760536a1 ("scripts: detect NIC firmware variant in a suitable location")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

conf/scripts/sockapi-ts
@@ -5,20 +5,18 @@
 # configuration.
 #
 . ${SF_TS_CONFDIR}/scripts/sfc_onload_gnu
+source "${TE_BASE}/scripts/lib"
 
 # Include the file if it really exists - this allows sapi-ts not to break.
 # It seems that the following functions may become unavailable on some
 # TE_TS_RIGSDIR implementations: 'scp_dir', 'ln_sf_safe'.
-[[ -e "${TE_TS_RIGSDIR}/scripts/lib.run" ]] \
-    && source "${TE_TS_RIGSDIR}/scripts/lib.run"
+source_if_exists "${TE_TS_RIGSDIR}/scripts/lib.run"
 
 . ${SF_TS_CONFDIR}/scripts/lib
 . ${SF_TS_CONFDIR}/scripts/ipvlan
 . ${SF_TS_CONFDIR}/scripts/sfptpd
 
-source "${TE_BASE}/scripts/lib"
-file_to_src="${TE_TS_RIGSDIR}/scripts/lib/ts_funcs"
-[[ -f "$src_file" ]] &&  source "$src_file"
+source_if_exists "${TE_TS_RIGSDIR}/scripts/lib/ts_funcs"
 
 #
 # The called function can export the following variables:

scripts/run.sh
@@ -12,15 +12,13 @@ popd >/dev/null
 
 source "${TE_BASE}/scripts/lib"
 source "${TE_BASE}/scripts/lib.grab_cfg"
-file_to_src="${TE_TS_RIGSDIR}/scripts/lib/grab_cfg_handlers"
-[[ -f "$file_to_src" ]] && source "$file_to_src"
+source_if_exists "${TE_TS_RIGSDIR}/scripts/lib/grab_cfg_handlers"
 
 # Include the file if it really exists - this allows sapi-ts not to break.
 # It seems that the following functions may become unavailable on some
 # TE_TS_RIGSDIR implementations: 'export_cmdclient', 'get_sfx_ifs' and
 # 'export_iut_fw_version'.
-[[ -e "${TE_TS_RIGSDIR}/scripts/lib.run" ]] \
-    && source "${TE_TS_RIGSDIR}/scripts/lib.run"
+source_if_exists "${TE_TS_RIGSDIR}/scripts/lib.run"
 
 source "${SF_TS_CONFDIR}/scripts/lib"
 