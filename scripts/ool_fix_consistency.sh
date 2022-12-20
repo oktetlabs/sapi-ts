oktetlabs/sapi-ts

commit cb3cc7367ea584e31efd9dfd17a0b340f84a765e

Bug 11959: ool_fix_consistency: avoid vlan tests on aggregated interfaces (intl)
Intel driver and old Mellanox one misbehave in cases of
testing over vlan using onload over af_xdp on aggregation interfaces.
There is no the problem on new Mellanox driver so we avoid such testing
on Intel NICs only.

scripts/ool_fix_consistency.sh
@@ -528,11 +528,13 @@ function af_xdp_fix()
             fi
         fi
 
-        # Avoid ipvlan/macvaln testing over vlan using onload over af_xdp on
-        # Intel/Mellanox NICs.
+        # Intel and Mellanox drivers misbehave in some cases.
         # Bug 11959.
         if [[ "$iut_drv" != "sfc" ]] ; then
             if ool_contains "onload" && ool_contains "vlan" ; then
+                # Avoid ipvlan/macvaln testing over vlan
+                # using onload over af_xdp on Intel/Mellanox NICs.
+                # Bug 11959.
                 if ool_contains "ipvlan" ; then
                     ool_remove "ipvlan" \
                         "$info/Bug 11959: avoid ipvlan testing over $bug_11959_restrictions"
@@ -540,6 +542,18 @@ function af_xdp_fix()
                 if ool_contains "macvlan" ; then
                     ool_remove "macvlan" \
                         "$info/Bug 11959: avoid macvlan testing over $bug_11959_restrictions"
+                fi
+                # Intel driver and old Mellanox one misbehave in cases of
+                # testing over vlan using onload over af_xdp on aggregation interfaces.
+                # There is no the problem on new Mellanox driver so we avoid such testing
+                # on Intel NICs only.
+                # Bug 11959.
+                if [[ "$iut_drv" == "i40e" ]] ; then
+                    if ool_contains "aggregation" ; then
+                            bug_11959_restrictions="${bug_11959_restrictions/\/Mellanox/}"
+                            ool_remove "aggregation" \
+                                "$info/Bug 11959: avoid aggregation testing over $bug_11959_restrictions"
+                    fi
                 fi
             fi
         fi

commit 2540944f6441386f9ff35fc870de69874fd8248c

scripts: avoid vlan option if testing onload af_xdp on Intel
Intel drivers i40e and ice misbehave when Onload over af_xdp over vlan
injects packets to the kernel. Cases with aggregation interfaces,
macvlan and ipvlan without vlan work fine thus we return them to
testing and disable only vlan requirement since now.

Fixes: cb3cc7367ea5 ("Bug 11959: ool_fix_consistency: avoid vlan tests on aggregated interfaces (intl)")
OL-Redmine-Id: 11959
Signed-off-by: Pavel Liulchak <pavel.liulchak@oktetlabs.ru>
Acked-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -506,7 +506,6 @@ function ipvlan_fix()
 function af_xdp_fix()
 {
     local info="af_xdp_fix"
-    local bug_11959_restrictions="vlan + onload + af_xdp on Intel/Mellanox NICs"
 
     ool_remove "af_xdp_common" \
         "$info: this option is not intended for standalone use"
@@ -546,33 +545,13 @@ function af_xdp_fix()
             fi
         fi
 
-        # Intel and Mellanox drivers misbehave in some cases.
+        # Both i40e and ice Intel drivers misbehave when Onload injects
+        # packets to kernel in case of onload + af_xdp + vlan
         # Bug 11959.
-        if [[ "$iut_drv" != "sfc" ]] ; then
+        if [[ "$iut_drv" == "i40e" ]] || [[ "$iut_drv" == "ice" ]]; then
             if ool_contains "onload" && ool_contains "vlan" ; then
-                # Avoid ipvlan/macvaln testing over vlan
-                # using onload over af_xdp on Intel/Mellanox NICs.
-                # Bug 11959.
-                if ool_contains "ipvlan" ; then
-                    ool_remove "ipvlan" \
-                        "$info/Bug 11959: avoid ipvlan testing over $bug_11959_restrictions"
-                fi
-                if ool_contains "macvlan" ; then
-                    ool_remove "macvlan" \
-                        "$info/Bug 11959: avoid macvlan testing over $bug_11959_restrictions"
-                fi
-                # Intel driver and old Mellanox one misbehave in cases of
-                # testing over vlan using onload over af_xdp on aggregation interfaces.
-                # There is no the problem on new Mellanox driver so we avoid such testing
-                # on Intel NICs only.
-                # Bug 11959.
-                if [[ "$iut_drv" == "i40e" ]] ; then
-                    if ool_contains "aggregation" ; then
-                            bug_11959_restrictions="${bug_11959_restrictions/\/Mellanox/}"
-                            ool_remove "aggregation" \
-                                "$info/Bug 11959: avoid aggregation testing over $bug_11959_restrictions"
-                    fi
-                fi
+                ool_remove "vlan" \
+                    "$info/Bug 11959: avoid Onload over af_xdp over vlan testing on Intel NICs"
             fi
         fi
     else