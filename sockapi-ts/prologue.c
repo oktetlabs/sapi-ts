oktetlabs/sapi-ts

commit e6d5823cf5eb1c66733068f654984f791bf198ca

sockapi-ts: skip nonaccel case if injection is disallowed
In case of non accelerated socket, we may encounter with
a situation when packets are injected to the kernel by Onload.
Such cases should be avoided if we disallow kernel injection
via inject_kernel_gid.

OL-Redmine-Id: 12887
Signed-off-by: Pavel Liulchak <pavel.liulchak@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

sockapi-ts/prologue.c
@@ -706,6 +706,40 @@ set_onload_module_params(void)
     }
 }
 
+/* The prologue reperesentation of inject_kernel_gid = -2 */
+#define PROLOGUE_DISALLOW_INJECT_ALWAYS -2
+
+/**
+ * Modifies requirement for the sockets that accelerate
+ * multicast traffic only (onload_socket_unicast_nonaccel).
+ *
+ * @param ta TA name.
+ *
+ * @return Status code.
+ */
+static te_errno
+fix_onload_unicast_nonaccel_req(const char *ta)
+{
+    int inject_kernel_gid = 0;
+    te_errno rc = 0;
+
+    rc = tapi_cfg_module_param_get_int(ta, "onload", "inject_kernel_gid",
+                                       &inject_kernel_gid);
+    if (rc != 0)
+        return rc;
+
+    if (inject_kernel_gid == PROLOGUE_DISALLOW_INJECT_ALWAYS)
+    {
+        RING("Avoid onload_unicast_nonaccel() socket function testing "
+             "if injection to the kernel is disallowed "
+             "(inject_kernel_gid = -2)");
+
+        rc = tapi_reqs_modify("!ONLOAD_NONACCEL");
+    }
+
+    return rc;
+}
+
 /**
  * Set Socket API library names for Test Agents in accordance
  * with configuration in configurator.conf.
@@ -932,6 +966,9 @@ main(int argc, char **argv)
             sockts_kmemleak_clear(ta);
         }
     }
+
+    TEST_STEP("Fix test requirements for the run");
+    CHECK_RC(fix_onload_unicast_nonaccel_req(pco_iut->ta));
 
     CHECK_RC(rc = cfg_tree_print(NULL, TE_LL_RING, "/:"));
 

commit c18b270b9a92b03dd09d2d4674ab79685abfe927

sockapi-ts: make onload related fixes in onload case only
The previous patch was wrong because we tried to get
onload parameter in both onload and pure Linux cases.
That crashed pure Linux cases. Now we check if the case is
onload or not and use IUT test agent name got from
environment variable.

Fixes: e6d5823cf5eb ("sockapi-ts: skip nonaccel case if injection is disallowed")
OL-Redmine-Id: 12887
Signed-off-by: Pavel Liulchak <pavel.liulchak@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

sockapi-ts/prologue.c
@@ -712,21 +712,19 @@ set_onload_module_params(void)
 /**
  * Modifies requirement for the sockets that accelerate
  * multicast traffic only (onload_socket_unicast_nonaccel).
- *
- * @param ta TA name.
- *
- * @return Status code.
  */
-static te_errno
-fix_onload_unicast_nonaccel_req(const char *ta)
+static void
+fix_onload_unicast_nonaccel_req(void)
 {
     int inject_kernel_gid = 0;
-    te_errno rc = 0;
+    const char *iut_ta = getenv("TE_IUT_TA_NAME");
 
-    rc = tapi_cfg_module_param_get_int(ta, "onload", "inject_kernel_gid",
-                                       &inject_kernel_gid);
-    if (rc != 0)
-        return rc;
+    if (iut_ta == NULL)
+        TEST_FAIL("TE_IUT_TA_NAME is not set");
+
+    CHECK_RC(tapi_cfg_module_param_get_int(iut_ta, "onload",
+                                           "inject_kernel_gid",
+                                           &inject_kernel_gid));
 
     if (inject_kernel_gid == PROLOGUE_DISALLOW_INJECT_ALWAYS)
     {
@@ -734,10 +732,8 @@ fix_onload_unicast_nonaccel_req(const char *ta)
              "if injection to the kernel is disallowed "
              "(inject_kernel_gid = -2)");
 
-        rc = tapi_reqs_modify("!ONLOAD_NONACCEL");
+        CHECK_RC(tapi_reqs_modify("!ONLOAD_NONACCEL"));
     }
-
-    return rc;
 }
 
 /**
@@ -967,8 +963,11 @@ main(int argc, char **argv)
         }
     }
 
-    TEST_STEP("Fix test requirements for the run");
-    CHECK_RC(fix_onload_unicast_nonaccel_req(pco_iut->ta));
+    if (tapi_onload_run())
+    {
+        TEST_STEP("Fix test requirements for the run");
+        fix_onload_unicast_nonaccel_req();
+    }
 
     CHECK_RC(rc = cfg_tree_print(NULL, TE_LL_RING, "/:"));
 