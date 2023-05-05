oktetlabs/sapi-ts

commit e6d5823cf5eb1c66733068f654984f791bf198ca

sockapi-ts: skip nonaccel case if injection is disallowed
In case of non accelerated socket, we may encounter with
a situation when packets are injected to the kernel by Onload.
Such cases should be avoided if we disallow kernel injection
via inject_kernel_gid.

OL-Redmine-Id: 12887
Signed-off-by: Pavel Liulchak <pavel.liulchak@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

sockapi-ts/prologue.c
@@ -706,6 +706,40 @@ set_onload_module_params(void)
     }
 }
 
+/* The prologue reperesentation of inject_kernel_gid = -2 */
+#define PROLOGUE_DISALLOW_INJECT_ALWAYS -2
+
+/**
+ * Modifies requirement for the sockets that accelerate
+ * multicast traffic only (onload_socket_unicast_nonaccel).
+ *
+ * @param ta TA name.
+ *
+ * @return Status code.
+ */
+static te_errno
+fix_onload_unicast_nonaccel_req(const char *ta)
+{
+    int inject_kernel_gid = 0;
+    te_errno rc = 0;
+
+    rc = tapi_cfg_module_param_get_int(ta, "onload", "inject_kernel_gid",
+                                       &inject_kernel_gid);
+    if (rc != 0)
+        return rc;
+
+    if (inject_kernel_gid == PROLOGUE_DISALLOW_INJECT_ALWAYS)
+    {
+        RING("Avoid onload_unicast_nonaccel() socket function testing "
+             "if injection to the kernel is disallowed "
+             "(inject_kernel_gid = -2)");
+
+        rc = tapi_reqs_modify("!ONLOAD_NONACCEL");
+    }
+
+    return rc;
+}
+
 /**
  * Set Socket API library names for Test Agents in accordance
  * with configuration in configurator.conf.
@@ -932,6 +966,9 @@ main(int argc, char **argv)
             sockts_kmemleak_clear(ta);
         }
     }
+
+    TEST_STEP("Fix test requirements for the run");
+    CHECK_RC(fix_onload_unicast_nonaccel_req(pco_iut->ta));
 
     CHECK_RC(rc = cfg_tree_print(NULL, TE_LL_RING, "/:"));
 

commit f42713679130f8f1e630e507fef974583c23216c

sockapi-ts: add missed headers into main prologue
Compiler (gcc (Debian 10.2.1-6) 10.2.1 20210110) printed warnings
about implicit declaration of functions. We have added necessary
headers to stop its complaining.

Fixes: e6d5823cf5eb ("sockapi-ts: skip nonaccel case if injection is disallowed")
OL-Redmine-Id: 12887
Signed-off-by: Pavel Liulchak <pavel.liulchak@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

sockapi-ts/prologue.c
@@ -21,8 +21,10 @@
 #include "tapi_test.h"
 #include "tapi_proc.h"
 #include "tapi_cfg_iptables.h"
+#include "tapi_cfg_modules.h"
 #include "tapi_network.h"
 #include "tapi_host_ns.h"
+#include "tapi_reqs.h"
 #include "onload.h"
 #include "lib-ts.h"
 #include "lib-ts_netns.h"