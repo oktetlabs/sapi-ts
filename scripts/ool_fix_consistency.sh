oktetlabs/sapi-ts

commit 2c05943d18d73e35c423467e376c5840c7c55a3c

scripts: disable testing netns_iut with af_xdp
AF_XDP socket from the secondary network namespace cannot access
a registered network interface from the primary network namespace.

OL-Redmine-Id: 12985
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -529,12 +529,14 @@ function pkt_nocomp_fix()
 
 function ipvlan_fix()
 {
-    local info="ipvlan_fix"
+    local info="ipvlan_fix/ST-2119 ipvlan should be tested with netns_iut only"
     if ool_contains "ipvlan" ; then
-        ool_replace "netns_all" "netns_iut" \
-            "$info/ST-2119 ipvlan should be tested with netns_iut only"
-        ool_add "netns_iut" \
-            "$info/ST-2119 ipvlan should be tested with netns_iut only"
+        if ! ool_contains "af_xdp*" ; then
+            ool_replace "netns_all" "netns_iut" "$info"
+            ool_add "netns_iut" "$info"
+        else
+            ool_replace "ipvlan" "macvlan" "$info"
+        fi
     fi
 }
 
@@ -603,6 +605,9 @@ function af_xdp_fix()
         # Scalable filters are not supported
         ool_remove "scalable*" "$info: scalable filters are not supported"
 
+        ool_replace "netns_iut" "netns_all" \
+            "$info/Bug 12985: af_xdp works with netns_all only"
+
         if ool_contains "zc_af_xdp*" ; then
             if [[ "$iut_drv" == "sfc" ]] ; then
                 if ool_contains "netns_*" ; then
@@ -642,6 +647,8 @@ function af_xdp_fix()
         if ool_contains "vlan" && \
             is_value_in_set "$iut_drv" "$vlan_af_xdp_problematic_drvs" ; then
             if ool_contains "aggregation" || ool_contains "team*" || ool_contains "bond*" ; then
+                # Note: testing of netns_iut is disabled due to Bug 12985,
+                # but let this code stay here.
                 if ool_contains "netns_iut" ; then
                     if ! ool_contains "macvlan" && ! ool_contains "ipvlan"; then
                         ool_replace "vlan" "macvlan" \

commit 616955d6e162b2a23e46bcb52af5edb531d770a6

scripts: disable testing of sub-interfaces in netns_all
Such a check for netns_all is already performed in gen_onload_part.py.
But compliance with this condition was violated by the commit 2c05943d1.

OL-Redmine-Id: 12985
Fixes: 2c05943d18d7 ("scripts: disable testing netns_iut with af_xdp")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -540,6 +540,27 @@ function ipvlan_fix()
     fi
 }
 
+#
+# netns_all moves/creates ALL tested interfaces into a separate network
+# namespace: unfortunately, Onload (onload_cp_server) cannot handle
+# the same if_indexes in different namespaces.
+#
+function netns_all_fix()
+{
+    local info="netns_all: test only real interfaces"
+
+    if ool_contains "netns_all" ; then
+        ool_remove "macvlan" "$info"
+        ool_remove "ipvlan" "$info"
+        ool_remove "vlan" "$info"
+        ool_remove "bond*" "$info"
+        ool_remove "team*" "$info"
+        ool_remove "aggregation" "$info"
+        unset SOCKAPI_TS_BOND
+    fi
+    netns_all_fix_done=true
+}
+
 # Intel i40e/ice and Mellanox mlx5_core drivers misbehave when Onload injects
 # packets to the kernel in case of onload + af_xdp + vlan.
 # Bug 11959.
@@ -548,6 +569,11 @@ vlan_af_xdp_problematic_drvs="i40e ice mlx5_core"
 function aggregation_fix()
 {
     local info="aggregation_fix"
+
+    if [[ -z "$netns_all_fix_done" ]] ; then
+        fail "${info}() should be after netns_all_fix()"
+    fi
+
     if ool_contains "aggregation" || ool_contains "team*" || ool_contains "bond*" ; then
         # aggregation interface + netns_iut has to be tested with either
         # macvlan/ipvlan or vlan
@@ -726,6 +752,7 @@ syscall_fix
 ef100soc_fix
 x3_fix
 build_ulhelper_fix
+netns_all_fix
 aggregation_fix
 af_xdp_fix
 # socket_cache_fix should be before scalable_fix