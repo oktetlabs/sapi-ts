oktetlabs/sapi-ts

commit c57e59da74a47f5020f617e987f776f15556a3c2

scripts: adjust periodic poll timer in non-int mode
Append 'frequent_periodic_poll' to ool list if there is any spin,
except int_spin. This sets higher frequency for periodic poll timer,
which is needed for sapi-ts tests in non-interrupt driven mode.

OL-Redmine-Id: 13257
Signed-off-by: Sergey Nikitin <sergey.nikitin@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -705,6 +705,16 @@ function af_xdp_fix()
             ool_remove "vlan" \
                 "$info/Bug 11959: $avoid_vlan_with_af_xdp_msg on Intel/Mellanox"
         fi
+
+        # Socket-tester tests need very frequent stack poll timer in
+        # non-interrupt driven mode. See Bug 13257.
+        if ool_contains "*spin" &&
+           ! ool_contains "int_spin" &&
+           ! ool_contains "int_driven"
+        then
+            ool_add "frequent_periodic_poll" \
+                "$info/Bug 13257: adjust periodic poll timer"
+        fi
     else
         # zc_af_xdp should be used only with AF_XDP
         ool_remove "zc_af_xdp" \

commit 180676c13c8ff72890a46fff3c5461c361dbb588

scripts: improve periodic poll when spinning
OL-Redmine-Id: 13376
Fixes: c57e59da74a4 ("scripts: adjust periodic poll timer in non-int mode")
Signed-off-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -708,9 +708,10 @@ function af_xdp_fix()
         # non-interrupt driven mode. See Bug 13257.
         if ool_contains "*spin" &&
            ! ool_contains "int_spin" &&
-           ! ool_contains "int_driven"
+           ! ool_contains "int_driven" &&
+           ! ool_contains "frequent_periodic_poll"
         then
-            ool_add "frequent_periodic_poll" \
+            ool_add "faster_periodic_poll" \
                 "$info/Bug 13257: adjust periodic poll timer"
         fi
     else