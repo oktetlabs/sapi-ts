Xilinx-CNS/cns-sapi-ts

commit 4a7e4624c8ea87e5aa674aadc60d6b2ae9ce8f25

scripts: turn off IF_DOWN_UP in case of sfc/i40e
sfc and i40e drivers do not restore XDP state after down-up thus
test with af_xdp + zc_af_xdp parameter combinations on intel/sfc
configurations fail and crash further testing. We avoid tests with
mentioned parameters and IF_DOWN_UP req on i40e and sfc
via setting !IF_DOWN_UP req.

OL-Redmine-Id: 12053
Signed-off-by: Pavel Liulchak <pavel.liulchak@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

scripts/ool_fix_reqs.py
@@ -128,6 +128,13 @@ add_req("!ONLOAD_ZC_SEND_USER_BUF",
 add_req("!ONLOAD_ZC_HLRX",
         "ON-14302: onload_zc_hlrx_recv_zc() is broken")
 
+# af_xdp + zc_af_xdp parameter combinations
+# on sfc/i40e crash further testing, see Bug-12053.
+if iut_drv in ["sfc", "i40e"]:
+    if "zc_af_xdp" in ools:
+        add_req("!IF_DOWN_UP",
+        "Bug-12053: sfc and i40e do not restore XDP state after down-up")
+
 # netns + zc_af_xdp parameter combination
 # on sfc crashes further testing, see SWNETLINUX-4809/Bug-11986.
 if iut_drv in ["sfc"]:

oktetlabs/sapi-ts

commit 17f96a1ab838e62054e37afd9b1a24ab86ab444b

revert: turn off IF_DOWN_UP in case of sfc/i40e
SSN now works with filters correctly, regardless of the state
of the interface.

OL-Redmine-Id: 12053
OL-Redmine-Id: 13394
Fixes: 4a7e4624c8ea ("scripts: turn off IF_DOWN_UP in case of sfc/i40e")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_reqs.py
@@ -134,13 +134,6 @@ add_req("!ONLOAD_ZC_SEND_USER_BUF",
 add_req("!ONLOAD_ZC_HLRX",
         "ON-14302: onload_zc_hlrx_recv_zc() is broken")
 
-# af_xdp + zc_af_xdp parameter combinations
-# on sfc/i40e crash further testing, see Bug-12053.
-if iut_drv in ["sfc", "i40e"]:
-    if "zc_af_xdp" in ools:
-        add_req("!IF_DOWN_UP",
-        "Bug-12053: sfc and i40e do not restore XDP state after down-up")
-
 # netns + zc_af_xdp parameter combination
 # on sfc crashes further testing, see SWNETLINUX-4809/Bug-11986.
 if iut_drv in ["sfc"]: