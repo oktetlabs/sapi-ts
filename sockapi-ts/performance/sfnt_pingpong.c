oktetlabs/sapi-ts

commit c20dfbde8f9029ee12895a836bcd6133886bb1a2

performance/sfnt_pingpong: fix for the stack reuse mode
Fix setting the EF_AF_XDP_TX_KICK_BATCH variable for the
--ool=reuse_stack mode. We have to use configurator to setup
env variables and restart 'pco_reuse_stack' RPC server in
order to our settings take effect.

Fixes: 6318325e1749 ("Bug 12300: sfnt_pingpong: fix the test for sfc+af_xdp+zerocopy case")
OL-Redmine-Id: 12811
Signed-off-by: Sergey Nikitin <sergey.nikitin@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

sockapi-ts/performance/sfnt_pingpong.c
@@ -83,8 +83,8 @@ main(int argc, char *argv[])
     int cpu_mask[] = {1};
     /* This value should match to cpu_mask value */
     const char *ef_periodic_timer_cpu = "1";
+    cfg_handle af_xdp_kick_batch_hdl = CFG_HANDLE_INVALID;
     char *saved_af_xdp_kick_batch = NULL;
-    te_bool restore_af_xdp_kick_batch = FALSE;
 
     TEST_START;
     TEST_GET_PCO(pco_iut);
@@ -121,17 +121,16 @@ main(int argc, char *argv[])
     {
         te_bool is_sfc;
 
-        saved_af_xdp_kick_batch = rpc_getenv(pco_iut, "EF_AF_XDP_TX_KICK_BATCH");
-
         rc = sockts_interface_is_sfc(pco_iut->ta, iut_if->if_name, &is_sfc);
         if (rc == 0 && is_sfc == TRUE)
         {
-            rpc_setenv(pco_iut, "EF_AF_XDP_TX_KICK_BATCH", "2", 1);
-            restore_af_xdp_kick_batch = TRUE;
+            af_xdp_kick_batch_hdl = sockts_set_env_gen(pco_iut,
+                                                       "EF_AF_XDP_TX_KICK_BATCH",
+                                                       "2", &saved_af_xdp_kick_batch,
+                                                       FALSE);
+            sockts_recreate_onload_stack(pco_iut);
+            rcf_rpc_server_restart(pco_iut);
         }
-
-        /* No need to restart RPC server since we've got new Onload instance
-         * when running sfnt-pingpong tool. */
     }
 
     TEST_STEP("Create client and server of sfnt-pingpong");
@@ -203,26 +202,8 @@ cleanup:
     te_string_free(&res);
     te_vec_free(&vec);
 
-    /* Restore EF_AF_XDP_TX_KICK_BATCH from the previous value, or unset if
-     * it was not set. */
-    if (restore_af_xdp_kick_batch == TRUE)
-    {
-        RPC_AWAIT_IUT_ERROR(pco_iut);
-
-        if (saved_af_xdp_kick_batch != NULL)
-        {
-            rc = rpc_setenv(pco_iut, "EF_AF_XDP_TX_KICK_BATCH",
-                            saved_af_xdp_kick_batch, 1);
-            free(saved_af_xdp_kick_batch);
-        }
-        else
-        {
-            rc = rpc_unsetenv(pco_iut, "EF_AF_XDP_TX_KICK_BATCH");
-        }
-
-        if (rc != 0)
-            MACRO_TEST_ERROR;
-    }
+    CLEANUP_CHECK_RC(sockts_restore_env_gen(pco_iut, af_xdp_kick_batch_hdl,
+                                            saved_af_xdp_kick_batch, FALSE));
 
     TEST_END;
 }

sockapi-ts/lib/sockapi-ts.h
@@ -3394,7 +3394,7 @@ sockts_init_msghdr(rpc_msghdr *msg, int length)
  * 
  * @note The function is inteded for use only in prologues/epilouges, as some
  * packages require changing EF_ variables. Affected packages: reuseport, bpf,
- * timestamps and multicast.
+ * timestamps, performance and multicast.
  *
  * @param pco_iut   IUT RPC server
  */

commit 597f92bc42d64d473ac742d31d06284f61b50db2

performance/sfnt_pingpong: use default kick settings
EF_AF_XDP_TX_KICK_BATCH is now 2 by default, so there is no need
to adjust it.

Fixes: c20dfbde8f90 ("performance/sfnt_pingpong: fix for the stack reuse mode")
Fixes: cc56928f0c8c ("Bug 12300: sfnt_pingpong: fix the test for sfc+af_xdp+zerocopy case")
Fixes: f6c14ad93ed0 ("performance/sfnt_pingpong: adjust test settings")
OL-Redmine-Id: 13384
Signed-off-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

sockapi-ts/performance/sfnt_pingpong.c
@@ -84,7 +84,6 @@ main(int argc, char *argv[])
     /* This value should match to cpu_mask value */
     const char *ef_periodic_timer_cpu = "1";
     cfg_handle af_xdp_kick_batch_hdl = CFG_HANDLE_INVALID;
-    char *saved_af_xdp_kick_batch = NULL;
 
     TEST_START;
     TEST_GET_PCO(pco_iut);
@@ -114,19 +113,6 @@ main(int argc, char *argv[])
      * It takes time, and sfnt-pingpong is sensitive to execution time.
      * See bug 12215.*/
     sockts_kill_zombie_stacks(pco_iut);
-
-    /* The test may fail with EINGPROGRESS on TCP iterations with the default
-     * value of EF_AF_XDP_TX_KICK_BATCH, so let's decrease it. It is acceptable
-     * workaround for sfnt_pingpong. See bug 12300#note-4 and bug 12811. */
-    if (proto == RPC_IPPROTO_TCP)
-    {
-        af_xdp_kick_batch_hdl = sockts_set_env_gen(pco_iut,
-                                                   "EF_AF_XDP_TX_KICK_BATCH",
-                                                   "2", &saved_af_xdp_kick_batch,
-                                                   FALSE);
-        sockts_recreate_onload_stack(pco_iut);
-        rcf_rpc_server_restart(pco_iut);
-    }
 
     TEST_STEP("Create client and server of sfnt-pingpong");
     CHECK_RC(tapi_sfnt_pp_create(cl_factory, sv_factory,
@@ -196,9 +182,6 @@ cleanup:
     CHECK_RC(tapi_sfnt_pp_destroy_server(server));
     te_string_free(&res);
     te_vec_free(&vec);
-
-    CLEANUP_CHECK_RC(sockts_restore_env_gen(pco_iut, af_xdp_kick_batch_hdl,
-                                            saved_af_xdp_kick_batch, FALSE));
 
     TEST_END;
 }