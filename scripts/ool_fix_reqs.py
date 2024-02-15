oktetlabs/sapi-ts

commit 17d6d6e5ae89b4339550e381f1bb0e4ca82059d1

revert: turn off IF_DOWN_UP in case of sfc/i40e
SSN now works with filters correctly, regardless of the state
of the interface.

OL-Redmine-Id: 12053
OL-Redmine-Id: 13394
Fixes: 4a7e4624c8ea ("scripts: turn off IF_DOWN_UP in case of sfc/i40e")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_reqs.py
@@ -134,13 +134,6 @@ add_req("!ONLOAD_ZC_SEND_USER_BUF",
 add_req("!ONLOAD_ZC_HLRX",
         "ON-14302: onload_zc_hlrx_recv_zc() is broken")
 
-# af_xdp + zc_af_xdp parameter combinations
-# on sfc/i40e crash further testing, see Bug-12053.
-if iut_drv in ["sfc", "i40e"]:
-    if "zc_af_xdp" in ools:
-        add_req("!IF_DOWN_UP",
-        "Bug-12053: sfc and i40e do not restore XDP state after down-up")
-
 # netns + zc_af_xdp parameter combination
 # on sfc crashes further testing, see SWNETLINUX-4809/Bug-11986.
 if iut_drv in ["sfc"]:

commit 2c911e1f9f68e98c9c426995e496bcdc8c961139

revert: revert: turn off IF_DOWN_UP in case of sfc/i40e
Testing has shown that it is still not worth down/up interfaces
together with Zero-Copy AF_XDP.

OL-Redmine-Id: 13463
Fixes: 17d6d6e5ae89 ("revert: turn off IF_DOWN_UP in case of sfc/i40e")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_reqs.py
@@ -134,6 +134,13 @@ add_req("!ONLOAD_ZC_SEND_USER_BUF",
 add_req("!ONLOAD_ZC_HLRX",
         "ON-14302: onload_zc_hlrx_recv_zc() is broken")
 
+# af_xdp + zc_af_xdp parameter combinations
+# on sfc/i40e crash further testing, see Bug-12053.
+if iut_drv in ["sfc", "i40e"]:
+    if "zc_af_xdp" in ools:
+        add_req("!IF_DOWN_UP",
+        "Bug-12053: sfc and i40e do not restore XDP state after down-up")
+
 # netns + zc_af_xdp parameter combination
 # on sfc crashes further testing, see SWNETLINUX-4809/Bug-11986.
 if iut_drv in ["sfc"]: