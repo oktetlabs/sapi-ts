oktetlabs/sapi-ts

commit da7d17988a675616f1cd3b5af36f24386b0539fb

scripts: disable zc_af_xdp in case of e810 with ice driver
Zero-copy af_xdp doesn't work on e810 NICs with ice driver.
To prevent breaking of night tests on e810 NICs, we temporarily
disable zc_af_xdp option.

OL-Redmine-Id: 12753
Signed-off-by: Pavel Liulchak <pavel.liulchak@oktetlabs.ru>
Acked-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -608,6 +608,11 @@ function af_xdp_fix()
                         "$info/SWNETLINUX-4809/Bug 11986: disable netns on SFC NICs with zc_af_xdp"
                 fi
             fi
+
+            if [[ "$iut_drv" == "ice" ]] ; then
+                ool_remove "zc_af_xdp" \
+                    "$info/Bug 12753: disable zc_af_xdp on Intel NICs with ice driver"
+            fi
         fi
 
         if ool_contains "tiny_spin" ; then

commit 421186c43159ce5028a79d459e1779320ce144b7

revert: disable zc_af_xdp in case of e810 with ice driver
Use zc_af_xdp on E810 to check the current status.

OL-Redmine-Id: 12753
OL-Redmine-Id: 13463
Fixes: da7d17988a67 ("scripts: disable zc_af_xdp in case of e810 with ice driver")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -660,11 +660,6 @@ function af_xdp_fix()
                 fi
             fi
 
-            if [[ "$iut_drv" == "ice" ]] ; then
-                ool_remove "zc_af_xdp" \
-                    "$info/Bug 12753: disable zc_af_xdp on Intel NICs with ice driver"
-            fi
-
             if [[ "$iut_drv" == "virtio-pci" ]] ; then
                 ool_remove "zc_af_xdp" \
                     "$info/Bug 12881: virtio_net driver does not support zerocopy mode"