oktetlabs/sapi-ts

commit 421186c43159ce5028a79d459e1779320ce144b7

revert: disable zc_af_xdp in case of e810 with ice driver
Use zc_af_xdp on E810 to check the current status.

OL-Redmine-Id: 12753
OL-Redmine-Id: 13463
Fixes: da7d17988a67 ("scripts: disable zc_af_xdp in case of e810 with ice driver")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -660,11 +660,6 @@ function af_xdp_fix()
                 fi
             fi
 
-            if [[ "$iut_drv" == "ice" ]] ; then
-                ool_remove "zc_af_xdp" \
-                    "$info/Bug 12753: disable zc_af_xdp on Intel NICs with ice driver"
-            fi
-
             if [[ "$iut_drv" == "virtio-pci" ]] ; then
                 ool_remove "zc_af_xdp" \
                     "$info/Bug 12881: virtio_net driver does not support zerocopy mode"

commit 8cfe6b74c01d657704bdf803f90962a47a5962c0

revert-2: disable zc_af_xdp in case of e810 with ice driver
Unfortunately, we still see the issue:
"NETDEV WATCHDOG: enp1s0f0 (ice): transmit queue 1 timed out".

OL-Redmine-Id: 12753
Fixes: 421186c43159 ("revert: disable zc_af_xdp in case of e810 with ice driver")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/ool_fix_consistency.sh
@@ -660,6 +660,11 @@ function af_xdp_fix()
                 fi
             fi
 
+            if [[ "$iut_drv" == "ice" ]] ; then
+                ool_remove "zc_af_xdp" \
+                    "$info/Bug 12753: disable zc_af_xdp on Intel NICs with ice driver"
+            fi
+
             if [[ "$iut_drv" == "virtio-pci" ]] ; then
                 ool_remove "zc_af_xdp" \
                     "$info/Bug 12881: virtio_net driver does not support zerocopy mode"