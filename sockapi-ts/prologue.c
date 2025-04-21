056c03a1cc4e6ac6499b3592a9097c9265dda3b0
Alexandra Kossovsky <Alexandra.Kossovsky@oktetlabs.ru>
2025-04-21 15:06:59 +0300

    prologue: copy register tool to TA
    Properly copy ssn_xdp_register tool to TA instead of using it from
    unknown location over NFS.
    
    Fixes: 26466faafd9e ("ts: move ssn_xdp_register into sapi-ts")
    Signed-off-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
    Acked-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

--- sockapi-ts/prologue.c
+++ sockapi-ts/prologue.c
@@ -299,6 +299,10 @@ copy_onload_tools(rcf_rpc_server *pco_iut)
                              "tools/ip/onload_stackdump",
                              "te_onload_stdump");
 
+            copy_onload_tool(pco_iut->ta, path, agt_dir,
+                             "tools/af_xdp/ssn_xdp_register",
+                             "ssn_xdp_register");
+
             /*
              * ulhepler build profile requires onload_helper tool
              * in the path
@@ -1226,6 +1230,7 @@ main(int argc, char **argv)
     TEST_GET_PCO(pco_tst);
     TEST_GET_IF(iut_if);
 
+    copy_onload_tools(pco_iut);
     if (branch_ssn())
     {
         TEST_STEP("For SSN, register IUT interfaces for XDP");
@@ -1275,7 +1280,6 @@ main(int argc, char **argv)
     }
 
     libts_timestamps_configure_sfptpd();
-    copy_onload_tools(pco_iut);
 
     /*
      * Flush neighbours tables in all hosts to
