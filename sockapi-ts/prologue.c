oktetlabs/sapi-ts

commit 26466faafd9e40e8cac40c33bb51c70dbe51c7ca

ts: move ssn_xdp_register into sapi-ts
OL-Redmine-Id: 14131
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

sockapi-ts/prologue.c
@@ -944,6 +944,162 @@ set_max_stacks_possible(rcf_rpc_server *rpcs)
 }
 
 /**
+ * Auxiliary structure to be used in the sapi_register_interfaces_for_xdp() function
+ * as a cookie.
+ */
+typedef struct ta_interfaces {
+    /** TA */
+    const char *ta;
+    /** String with interface names */
+    te_string *interfaces;
+} ta_interfaces;
+
+static te_errno
+sapi_aggr_name_get(const char *ta, const char *ifname, char **name)
+{
+    te_errno        rc;
+    unsigned int    i;
+    unsigned int    n_handles;
+    cfg_handle     *handles = NULL;
+    char           *str = NULL;
+    char           *oid_str = NULL;
+    const char     *aggr_name = NULL;
+
+    CHECK_EXPR(cfg_find_pattern_fmt(&n_handles, &handles,
+                                "/agent:%s/aggregation:*/interface:", ta));
+
+    for (i = 0; i < n_handles; i++)
+    {
+        CHECK_EXPR(cfg_get_instance(handles[i], NULL, &str));
+
+        if (strcmp(ifname, str) == 0)
+        {
+            cfg_oid *oid = NULL;
+
+            CHECK_EXPR(cfg_get_oid_str(handles[i], &oid_str));
+            oid = cfg_convert_oid_str(oid_str);
+            if (oid == NULL)
+            {
+                rc = TE_EINVAL;
+                goto cleanup;
+            }
+
+            /* "agent:<ta_name>/aggregation:<aggr_name>/interface:" */
+            aggr_name = CFG_OID_GET_INST_NAME(oid, 2);
+            *name = strdup(aggr_name);
+
+            FREE_AND_CLEAN(str);
+            FREE_AND_CLEAN(oid_str);
+            cfg_free_oid(oid);
+            break;
+        }
+        FREE_AND_CLEAN(str);
+    }
+
+    if (aggr_name == NULL)
+    {
+        ERROR("tapi_cfg_aggr_foreach_member() failed find aggregation "
+              "name for: %s", ifname);
+        rc = TE_EINVAL;
+    }
+
+cleanup:
+    free(handles);
+    free(oid_str);
+    free(str);
+
+    return rc;
+}
+
+static te_errno
+sapi_get_aggr_interfaces(const char *ta, const char *aggr_ifname,
+                         te_string *interfaces)
+{
+    te_errno        rc;
+    unsigned int    i;
+    unsigned int    n_handles;
+    cfg_handle     *handles;
+    char           *aggr_name = NULL;
+
+    CHECK_EXPR(cfg_find_pattern_fmt(&n_handles, &handles,
+                                    "/agent:%s/aggregation:%s/member:*",
+                                    ta, aggr_ifname));
+
+    for (i = 0; i < n_handles; i++)
+    {
+        char *member = NULL;
+        CHECK_EXPR(cfg_get_inst_name(handles[i], &member));
+        te_string_append(interfaces, " %s", member);
+        free(member);
+    }
+
+cleanup:
+    return rc;
+}
+
+
+/**
+ * Callback for sapi_register_interfaces_for_xdp() to count interfaces.
+ */
+static te_errno
+sapi_get_nut_interfaces(cfg_net_t *net, cfg_net_node_t *node,
+                        const char *oid_str, cfg_oid *oid, void *cookie)
+{
+    const char *ta;
+    const char *ifname;
+    ta_interfaces *ta_ifs = (ta_interfaces *)cookie;
+
+    UNUSED(net);
+
+    if (strcmp(cfg_oid_inst_subid(oid, 1), "agent") != 0 ||
+        strcmp(cfg_oid_inst_subid(oid, 2), "interface") != 0)
+    {
+        ERROR("Network node '%s' is not an interface", oid_str);
+        return TE_RC(TE_TAPI, TE_ENOENT);
+    }
+
+    ta = CFG_OID_GET_INST_NAME(oid, 1);
+    if (strcmp(ta, ta_ifs->ta) == 0 && node->type == NET_NODE_TYPE_NUT)
+    {
+        ifname = CFG_OID_GET_INST_NAME(oid, 2);
+        te_string_append(ta_ifs->interfaces, " %s", ifname);
+    }
+
+    return 0;
+}
+
+static te_errno
+sapi_register_interfaces_for_xdp(rcf_rpc_server *rpcs)
+{
+    te_errno rc;
+    te_string cmd = TE_STRING_INIT;
+    te_string interfaces = TE_STRING_INIT;
+    ta_interfaces ta_ifs = {.ta = rpcs->ta, .interfaces = &interfaces};
+    char *bond_name = getenv("SOCKAPI_TS_BOND_NAME");
+    char *xdp_flow_filters = getenv("EF_AF_XDP_ENABLE_FLOW_FILTERS");
+
+    te_string_append(&cmd, "%s --skip-none", SSN_XDP_REGISTER);
+
+    if (xdp_flow_filters != NULL && strcmp(xdp_flow_filters, "0") == 0)
+        te_string_append(&cmd, " %s", "-n1");
+
+    if (bond_name == NULL)
+        CHECK_EXPR(tapi_cfg_net_foreach_node(sapi_get_nut_interfaces, &ta_ifs));
+    else
+        CHECK_EXPR(sapi_get_aggr_interfaces(rpcs->ta, bond_name, &interfaces));
+
+    te_string_append(&cmd, " %s", interfaces.ptr);
+
+    rpc_system(rpcs, cmd.ptr);
+
+cleanup:
+    te_string_free(&cmd);
+    te_string_free(&interfaces);
+
+    return rc;
+}
+
+/**
  * Set Socket API library names for Test Agents in accordance
  * with configuration in configurator.conf.
  *
@@ -1055,6 +1211,12 @@ main(int argc, char **argv)
     TEST_GET_PCO(pco_iut);
     TEST_GET_PCO(pco_tst);
     TEST_GET_IF(iut_if);
+
+    if (branch_ssn())
+    {
+        TEST_STEP("For SSN, register IUT interfaces for XDP");
+        CHECK_RC(sapi_register_interfaces_for_xdp(pco_iut));
+    }
 
     CHECK_RC(set_max_stacks_possible(pco_iut));
 

sockapi-ts/epilogue.c
@@ -26,6 +26,14 @@
     "/parser:"SERIAL_LOG_PARSER_NAME        \
     "/event:onload_banners/counter:"
 
+/*
+ * ssn_xdp_register may encounter the removal of several zombie stacks,
+ * this may take some time.
+ */
+#define SSN_XDP_UNREG_WAIT_TIMEOUT_MS   60000
+#define SSN_XDP_UNREG_RETRY_TIMEOUT_MS  500
+#define SSN_XDP_UNREG_RETRIES_NUM       10
+
 /**
  * Stop nfq_daemon on @p pco.
  *
@@ -117,6 +125,36 @@ sockts_check_zombie_stack(rcf_rpc_server *rpcs)
     }
 }
 
+static void
+ssn_xdp_unregister(rcf_rpc_server *rpcs)
+{
+    unsigned int i;
+    rpc_wait_status rc;
+
+    for (i = 0; i < SSN_XDP_UNREG_RETRIES_NUM; i++)
+    {
+        rpcs->timeout = SSN_XDP_UNREG_WAIT_TIMEOUT_MS;
+
+        RPC_AWAIT_IUT_ERROR(rpcs);
+        rc = rpc_system_ex(rpcs, "%s --unregister --zombie", SSN_XDP_REGISTER);
+        if (rc.value == 0)
+            break;
+
+        if (i < SSN_XDP_UNREG_RETRIES_NUM)
+        {
+            te_motivated_msleep(SSN_XDP_UNREG_RETRY_TIMEOUT_MS,
+                                "wait for the next attempt to ungregistering");
+        }
+    }
+
+    if (rc.value != 0)
+    {
+        ERROR("%s() failed to unregister interfaces on %s after %u "
+              "attempts", __func__, rpcs->ta, i);
+        ERROR_VERDICT("Unable to unregister interfaces");
+    }
+}
+
 /**
  * Cleanup.
  *
@@ -193,6 +231,35 @@ main(int argc, char **argv)
         if (kill_zombie_stacks != NULL &&
             strcmp(kill_zombie_stacks, "yes") == 0)
             sockts_kill_check_zombie_stack(pco_iut, TRUE);
+    }
+
+
+    if (branch_ssn())
+    {
+        rcf_rpc_server *pco_reuse_stack = NULL;
+        char *ef_name = rpc_getenv(pco_iut, "EF_NAME");
+
+        TEST_STEP("Unregister IUT interfaces for XDP with "
+                  "forced removal of zombie stacks");
+        if (!te_str_is_null_or_empty(ef_name))
+        {
+            rc = rcf_rpc_server_get(pco_iut->ta, "pco_reuse_stack",
+                                    NULL, RCF_RPC_SERVER_GET_EXISTING,
+                                    &pco_reuse_stack);
+            if (rc == 0)
+            {
+                TEST_SUBSTEP("Remove the RCF RPC server started for stack "
+                             "reuse");
+                CHECK_RC(rcf_rpc_server_destroy(pco_reuse_stack));
+                /* FIXME: remove this after OL-Bug 14171 */
+                sockts_kill_zombie_stacks_gen(pco_iut, 0);
+            }
+            else if (TE_RC_GET_ERROR(rc) != TE_ENOENT)
+            {
+                TEST_FAIL("rcf_rpc_server_get() returned unexpected error");
+            }
+        }
+        ssn_xdp_unregister(pco_iut);
     }
 
     unlink(name_p);

sockapi-ts/lib/sockapi-ts.h
@@ -4157,6 +4157,13 @@ extern void
 sockts_destroy_netns(const char *ta, rcf_rpc_server *rpcs_ns,
                      const char *netns, const char *netns_ta);
 
+static inline bool branch_ssn()
+{
+    char *ool_branch = getenv("OOL_BRANCH");
+
+    return (ool_branch != NULL && strcmp(ool_branch, "ssn") == 0);
+}
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

commit 1a530d17538cf08ad98ae6aec4987d495ad3c9c7

prologue: copy register tool to TA
Properly copy ssn_xdp_register tool to TA instead of using it from
unknown location over NFS.

Fixes: 26466faafd9e ("ts: move ssn_xdp_register into sapi-ts")
Signed-off-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Acked-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

sockapi-ts/prologue.c
@@ -291,6 +291,10 @@ copy_onload_tools(rcf_rpc_server *pco_iut)
                              "tools/ip/onload_stackdump",
                              "te_onload_stdump");
 
+            copy_onload_tool(pco_iut->ta, path, agt_dir,
+                             "tools/af_xdp/ssn_xdp_register",
+                             "ssn_xdp_register");
+
             /*
              * ulhepler build profile requires onload_helper tool
              * in the path
@@ -1218,6 +1222,7 @@ main(int argc, char **argv)
     TEST_GET_PCO(pco_tst);
     TEST_GET_IF(iut_if);
 
+    copy_onload_tools(pco_iut);
     if (branch_ssn())
     {
         TEST_STEP("For SSN, register IUT interfaces for XDP");
@@ -1267,7 +1272,6 @@ main(int argc, char **argv)
     }
 
     libts_timestamps_configure_sfptpd();
-    copy_onload_tools(pco_iut);
 
     /*
      * Flush neighbours tables in all hosts to